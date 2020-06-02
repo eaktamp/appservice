1017370f14273f58e15ea0c93f35137a43685583
eaktamp <eaktamp@gmail.com>
2020-06-02 15:40:06 +0700

    updateedit

--- checkdata.php
+++ checkdata.php
@@ -1,3 +1,22 @@
+<?php
+date_default_timezone_set("Asia/Bangkok");
+function alphanumeric_rand($num_require=8) {
+  $alphanumeric = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9);
+  if($num_require > sizeof($alphanumeric)){
+    echo "Error alphanumeric_rand(\$num_require) : \$num_require must less than " . sizeof($alphanumeric) . ", $num_require given";
+    return;
+}
+$rand_key = array_rand($alphanumeric , $num_require);
+for($i=0;$i<sizeof($rand_key);$i++) $randomstring .= $alphanumeric[$rand_key[$i]];
+  return $randomstring;
+}
+$rd = alphanumeric_rand(12);
+$r5 = substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ23456789'),0,5);
+$fc = DATE('YmdHis');
+
+$link = $rd.MD5($fc).$r5;
+
+?>
 <!DOCTYPE html>
 <html lang="th">
 <head>
@@ -44,11 +63,14 @@
         //echo $have_user_yet['hn'];
 
         if ($count > 0) {
-            session_start();
+           // session_start();
             $accoutUsser = mysqli_fetch_assoc($have_user_yet);
-            $_SESSION['hn']  =  $accoutUsser['hn'];
+            $_POST['hn']  =  $accoutUsser['hn'];
             //echo ' <br>';
-            $_SESSION['cid'] = $accoutUsser['cid'];
+            $_POST['cid'] = $accoutUsser['cid'];
+
+            $hn  =  $_POST['hn'];
+            $cid =  $_POST['cid'];
 
             /*echo $_POST['hn']  =  $accoutUsser['hn'];
             echo ' <br>';
@@ -56,14 +78,19 @@
             */
             //$password = $con->real_escape_string md5((md5($_POST['cid'])));//decode
             echo "<script>alertify.success('พบข้อมูล');</script>";
-            header('location:senddata.php');
+           // header('location:senddata.php');
+            ?>
+            <script type="text/javascript">
+                window.location.href = "senddata.php?<?=$link;?>&cid=<?=$cid;?>&hn=<?=$hn;?>&<?=$link;?>";
+            </script>
+            <?php
         } else {
             echo "<script>alertify.error('ไม่พบข้อมูลผู้ป่วย');</script>";
         }
     }
     ?>
 
-      
+
 </body>
 
 </html>
