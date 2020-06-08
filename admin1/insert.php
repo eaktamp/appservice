934016be2ca8c8f7a00fb5f9c9ff75c39756a3fa
unknown <tao32040@gmail.com>
2020-06-08 09:46:20 +0700

    update log patient

--- admin1/insert.php
+++ admin1/insert.php
@@ -26,58 +26,39 @@
   $amphoe    = $_POST['amphoe'];
   $province  = $_POST['province'];
   $zipcode   = $_POST['zipcode'];
-  //$qcode     = ""; 
-  //$keycode   = "";
-  //$modify    = "";
-  //$status    = "1";
   $flage     = "1";
-  //$fileimg   = "";
+
   $dateupdate = DATE('Y-m-d H:i:s');
   $ipupdate   = $_SERVER['REMOTE_ADDR'];
   $cdate    = DATE('Y-m-d');
   $ctime    = DATE('H:i:s');
 
 
-  // if ($csum < "7") {
-  //     $csum =  "1";
-  // } elseif ($csum > "6" && $csum < "12" ) {
-  //     $csum = "2";
-  // } elseif ($csum > "11") {
-  //     $csum = "3";
-  // }
-  // 	$score = $csum;
-
-  // if ($csumb < "7") {
-  //     $csumb =  "score_a";
-  // } elseif ($csumb > "6" && $csumb < "12" ) {
-  //     $csumb = "score_b";
-  // } elseif ($csumb > "11") {
-  //     $csumb = "score_c";
-  // }
-  // 	$scoresum = $csumb;
-
   $searchuser = "SELECT * FROM web_data_patient where cid = '" . $cid . "'  ";
   $have_user_yet = mysqli_query($conf, $searchuser);
   $count = mysqli_num_rows($have_user_yet);
   // $have_user_yet['cid'];
   if ($count > 0) {
-      echo  $log = "UPDATE  web_data_patient SET hn = '$hn',cid = '$cid',fname = '$fname',lname = '$lname',phone='$phone'
+        $log = "UPDATE  web_data_patient SET hn = '$hn',cid = '$cid',fname = '$fname',lname = '$lname',phone='$phone'
        ,pttype='$pttype',lineid='$lineid',adddess='$adddess',moo='$moo',district='$district',amphoe='$amphoe',province='$province'
        ,zipcode='$zipcode',flage='$flage',dateupdate='$dateupdate',ipupdate='$ipupdate',cdate='$cdate',ctime='$ctime'
       WHERE cid = '$cid'";
     $query = mysqli_query($conf, $log);
-    //header("Location: app.php?cid=$cid&hn=$hn");
-    header("Location: app.php?hn=$hn"); //เอาค่า params ที่ส่งไปออกเพื่อให้ link clean ขึ้น
+
+    if($query){
+      $update_date = "INSERT INTO web_data_patient_log (user,patient_hn,ipupdate,dateupdate) VALUES ('$admininsert_id','$hn','$ipupdate','".date("Y-m-d H:i:s")."')";
+      $queryLog = mysqli_query($conf, $update_date);
+    }
+
+    header("Location: app.php?hn=$hn"); 
     mysqli_close($conf);
   } else {
-   echo $log = "INSERT INTO web_data_patient (hn,cid,fname,lname,phone,pttype,lineid,adddess,moo,district,amphoe,province,zipcode,flage,dateupdate,ipupdate,cdate,ctime,user_insert,user_insert_id) 
+    $log = "INSERT INTO web_data_patient (hn,cid,fname,lname,phone,pttype,lineid,adddess,moo,district,amphoe,province,zipcode,flage,dateupdate,ipupdate,cdate,ctime,user_insert,user_insert_id) 
                     VALUES ('$hn','$cid','$fname','$lname','$phone','$pttype','$lineid','$adddess','$moo','$district','$amphoe','$province','$zipcode','$flage','$dateupdate','$ipupdate','$cdate','$ctime','$admininsert','$admininsert_id' )";
     $query = mysqli_query($conf, $log);
     header("Location: app.php?hn=$hn");
     mysqli_close($conf);
   }
-
-
   ?>
 </body>
 
