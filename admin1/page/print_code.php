3a8b4e9997a1a176b86c20cee39f85dbe22ba6f7
eaktamp <eaktamp@gmail.com>
2020-06-15 10:51:37 +0700

    bartest

---
+++ admin1/page/print_code.php
@@ -0,0 +1,127 @@
+<?php 
+session_start();
+ob_start();
+$admininsert =  $_SESSION['qfname'].' '. $_SESSION['qlname'];
+$admininsert_id =  $_SESSION['username'];
+date_default_timezone_set("Asia/Bangkok");
+require_once('mpdf/mpdf.php');
+include "../../config/web_con.php";
+/*
+$fname      = $_POST['fname'];
+$lname      = $_POST['lname'];
+$adddess    = $_POST['adddess'];
+$moo        = $_POST['moo'];
+$district   = $_POST['district'];
+$amphoe     = $_POST['amphoe'];
+$province   = $_POST['province'];
+$zipcode    = $_POST['zipcode'];
+$phone       = $_POST['phone'];
+$file       = $_POST['hn'];
+*/
+$file       = $_POST['hn'];
+$code       = "RF436370542TH";
+$selectaddresspt = "SELECT * FROM web_data_patient  where hn = '$file'";
+$queryAdpt= mysqli_query($conf,$selectaddresspt);
+$resultaddressPt = mysqli_fetch_array($queryAdpt);
+   $fname      = $resultaddressPt['fname'];
+   $lname      = $resultaddressPt['lname'];
+   $adddess    = $resultaddressPt['adddess'];
+   $moo        = $resultaddressPt['moo'];
+   $district   = $resultaddressPt['district'];
+   $amphoe     = $resultaddressPt['amphoe'];
+   $province   = $resultaddressPt['province'];
+   $zipcode    = $resultaddressPt['zipcode'];
+   $hn         = $resultaddressPt['hn'];
+   $phone      = $resultaddressPt['phone'];
+   $ipupdate   = $_SERVER['REMOTE_ADDR'];
+   $page_flage = "Admin_Print";
+
+   $update_date = "INSERT INTO web_data_patient_log (user,patient_hn,ipupdate,dateupdate,page_flage) VALUES ('$admininsert_id','$hn','$ipupdate','".date("Y-m-d H:i:s")."','$page_flage')";
+   $queryLog = mysqli_query($conf, $update_date);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/print.css">
+</head>
+<body>
+
+
+<div class="div3">
+	<table width = 60% style="border:1px #000000;"  bgcolor="#000000"><tr><td style="border:1px dashed white;" bgcolor="white" align="center">
+		<div align="center" style="font-size: 12px"><?php echo "ชำระค่าฝากส่งเป็นรายเดือน" ?></div>
+		<div align="center" style="font-size: 12px"><?php echo "ใบอนุญาตเลขที่ 27/2521" ?></div>
+		<div align="center" style="font-size: 12px"><?php echo "ปทจ.ปราจีนบุรี" ?></div>
+	</td></tr></table>
+</div>
+<div class="div4">
+	<table width = 100% style="border:1px #000000;"  bgcolor="#000000"><tr><td style="border:1px dashed white;" bgcolor="white" align="center">
+		<div align="center" style="font-size: 25px"><?php echo "ยาและเวชภัณฑ์" ?></div>
+		<div align="center" style="font-size: 14px"><?php echo "โรงพยาบาลเจ้าพระยาอภัยภูเบศร" ?></div>
+		<div align="center" style="font-size: 14px"><?php echo "(หลีกเลี่ยงแสงแดดและความชื้น) (ระวังการกระแทก)" ?></div>
+	</table>
+</div>
+
+	<div class="div1">
+		<div style="font-size: 12px"><?php echo "ฝ่ายเภสัชกรรม"; ?></div>
+		<div style="font-size: 12px"><?php echo "โรงพยาบาลเจ้าพระยาอภัยภูเบศร"; ?></div>
+		<div style="font-size: 12px"><?php echo "เลขที่ 32/7 หมู่ 12 ตำบลท่างาม"; ?></div>
+		<div style="font-size: 12px"><?php echo "อำเภอเมือง จังหวัดปราจีนบุรี"; ?></div>
+		<div style="font-size: 12px"><?php echo "25000"; ?></div>
+	</div>
+
+	<div class="div2" >
+		<div style="font-size: 15px"><?php echo "ชื่อที่อยู่ผู้รับ"; ?></div>
+		<div style="font-size: 20px"><?php echo "คุณ. ".$fname." ".$lname." (".$phone.")"; ?></div>
+		<div style="font-size: 20px"><?php echo "เลขที่ ".$adddess." หมู่ ".$moo." ตำบล".$district; ?></div>
+		<div style="font-size: 20px"><?php echo "อำเภอ".$amphoe." จังหวัด".$province; ?></div>
+		<div style="font-size: 20px"><?php echo $zipcode; ?></div>
+	</div>
+	<div class="div6">
+		<div style="font-size: 13px"><?php echo "จำนวน ......... รายการ"; ?></div>
+		<br><BR>
+		<!-- <div style="font-size: 13px"><?php //echo "&nbsp;&nbsp;&nbsp;ติดสติ๊กเกอร์"; ?></div> -->
+		<div style='text-align: center; font-size: 13px;'>
+		<!-- <img src="rthaipost.png" alt="" width="250px" higth="80px"> -->
+   		<img alt='Barcode Generator TEC-IT'
+       src='https://barcode.tec-it.com/barcode.ashx?data=<?=$code;?>&code=&multiplebarcodes=true&translate-esc=true&unit=Fit&dpi=72&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=0'/>
+</div>
+	
+<div style='padding-top:8px; text-align:center; font-size:15px; font-family: Source Sans Pro, Arial, sans-serif;'>
+</div>
+
+	</div>
+	<div class="div5">
+		<div align="center" style="font-size: 12px"><?php echo "รับรองที่อยู่ถูกต้อง"; ?></div>
+		<br><br>
+		<div style="font-size: 10px"><?php echo "ลงชื่อ ..................................................................."; ?></div>
+
+	</div>
+
+</body>
+</html>
+
+<?php 
+
+
+//$filel = date('Y-m-d_His');
+$filel = $file;
+
+$save = "pdf/".$filel.".pdf";
+$lo   = "Location:pdf/".$filel.".pdf";
+
+$html = ob_get_contents();
+ob_end_clean();
+$pdf = new mPDF('th','A4','0','THSaraban');
+$pdf->SetDisplayMode('fullpage');
+$stylesheet = file_get_contents('css/print.css');
+$pdf->WriteHTML($stylesheet,1);
+$pdf->WriteHTML($html,2);
+$success = $pdf->Output($save);
+header($lo);
+die();
+
+
+?>
